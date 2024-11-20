cafe6bfca18294aeb1e1f89b03aaa588ea8983cb
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-11-20 11:49:30 +0100

    rm app

--- src/main/java/ma/ensa/mutuelle/MutuelleApplication.java
+++ src/main/java/ma/ensa/mutuelle/MutuelleApplication.java
@@ -1,13 +1,28 @@
 package ma.ensa.mutuelle;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MutuelleApplication {
+public class MutuelleApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MutuelleApplication.class, args);
-	}
+    @Autowired
+    private JobLauncher jobLauncher;
 
+    @Autowired
+    private Job importJob;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutuelleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        jobLauncher.run(importJob, new JobParametersBuilder().toJobParameters());
+    }
 }
--- src/main/java/ma/ensa/mutuelle/config/BatchConfig.java
+++ src/main/java/ma/ensa/mutuelle/config/BatchConfig.java
@@ -3,9 +3,7 @@ package ma.ensa.mutuelle.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.persistence.EntityManagerFactory;
-import ma.ensa.mutuelle.listeners.JobCompletionNotificationListener;
 import ma.ensa.mutuelle.models.Dossier;
-import ma.ensa.mutuelle.models.MedicamentReferentiel;
 import ma.ensa.mutuelle.repositories.MedicamentReferentielRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -13,6 +11,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -25,22 +24,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
+    @Bean
+    public JobBuilderFactory jobBuilderFactory(JobRepository jobRepository) {
+        return new JobBuilderFactory(jobRepository);
+    }
 
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
+    @Bean
+    public StepBuilderFactory stepBuilderFactory(JobRepository jobRepository) {
+        return new StepBuilderFactory(jobRepository);
+    }
 
-    @Autowired
-    private MedicamentReferentielRepository medicamentReferentielRepository;
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
     @Bean
     public JsonItemReader<Dossier> dossierReader() {
+        System.out.println("the reader");
+     
         return new JsonItemReaderBuilder<Dossier>()
                 .jsonObjectReader(new JacksonJsonObjectReader<>(Dossier.class))
                 .resource(new ClassPathResource("folders.json"))
@@ -51,86 +60,40 @@ public class BatchConfig {
     @Bean
     public ItemProcessor<Dossier, Dossier> dossierProcessor() {
         return dossier -> {
-            // Validation logic
-            if (dossier.getNomAssure() == null || dossier.getNomAssure().isEmpty()) {
-                throw new IllegalArgumentException("Nom de l'assuré ne doit pas être vide");
-            }
-            if (dossier.getNumeroAffiliation() == null || dossier.getNumeroAffiliation().isEmpty()) {
-                throw new IllegalArgumentException("Numéro d'affiliation ne doit pas être vide");
-            }
-            if (dossier.getPrixConsultation() == null || dossier.getPrixConsultation() <= 0) {
-                throw new IllegalArgumentException("Prix de la consultation doit être positif");
-            }
-            if (dossier.getMontantTotalFrais() == null || dossier.getMontantTotalFrais() <= 0) {
-                throw new IllegalArgumentException("Montant total des frais doit être positif");
-            }
-            if (dossier.getTraitements() == null || dossier.getTraitements().isEmpty()) {
-                throw new IllegalArgumentException("La liste des traitements doit être présente et non vide");
-            }
+            // Print the DTO after reading
+            System.out.println("the processor");
+            System.out.println(dossier);
             return dossier;
         };
     }
 
-   @Bean
-public ItemWriter<Dossier> dossierWriter(EntityManagerFactory entityManagerFactory) {
-    JpaItemWriter<Dossier> writer = new JpaItemWriter<>();
-    writer.setEntityManagerFactory(entityManagerFactory);
-    return writer;
-}
-
     @Bean
-    public JsonItemReader<MedicamentReferentiel> medicamentReferentielReader() {
-        return new JsonItemReaderBuilder<MedicamentReferentiel>()
-                .jsonObjectReader(new JacksonJsonObjectReader<>(MedicamentReferentiel.class))
-                .resource(new ClassPathResource("medicaments.json"))
-                .name("medicamentReferentielJsonItemReader")
-                .build();
-    }
-
-    @Bean
-    public ItemProcessor<MedicamentReferentiel, MedicamentReferentiel> medicamentReferentielProcessor() {
-        return medicamentReferentiel -> {
-            // Implement your processing logic here
-            return medicamentReferentiel;
+    public ItemWriter<Dossier> dossierWriter() {
+        return dossiers -> {
+            System.out.println("the writer");
+            for (Dossier dossier : dossiers) {
+                System.out.println(dossier);
+            }
         };
     }
 
     @Bean
-    public ItemWriter<MedicamentReferentiel> medicamentReferentielWriter() {
-        return new RepositoryItemWriterBuilder<MedicamentReferentiel>()
-                .repository(medicamentReferentielRepository)
-                .methodName("save")
-                .build();
-    }
-
-    @Bean
-    public Job importJob(JobCompletionNotificationListener listener, Step dossierStep, Step medicamentReferentielStep) {
-        return jobBuilderFactory.get("importJob")
-                .incrementer(new RunIdIncrementer())
-                .listener(listener)
-                .flow(dossierStep)
-                .next(medicamentReferentielStep)
-                .end()
-                .build();
-    }
-
-    @Bean
-    public Step dossierStep(ItemReader<Dossier> reader, ItemProcessor<Dossier, Dossier> processor, ItemWriter<Dossier> writer) {
+    public Step dossierStep(StepBuilderFactory stepBuilderFactory, PlatformTransactionManager transactionManager, ItemReader<Dossier> reader, ItemProcessor<Dossier, Dossier> processor, ItemWriter<Dossier> writer) {
         return stepBuilderFactory.get("dossierStep")
                 .<Dossier, Dossier>chunk(10)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
+                .transactionManager(transactionManager)
                 .build();
     }
 
     @Bean
-    public Step medicamentReferentielStep(ItemReader<MedicamentReferentiel> reader, ItemProcessor<MedicamentReferentiel, MedicamentReferentiel> processor, ItemWriter<MedicamentReferentiel> writer) {
-        return stepBuilderFactory.get("medicamentReferentielStep")
-                .<MedicamentReferentiel, MedicamentReferentiel>chunk(10)
-                .reader(reader)
-                .processor(processor)
-                .writer(writer)
+    public Job importJob(JobBuilderFactory jobBuilderFactory, Step dossierStep) {
+        return jobBuilderFactory.get("importJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(dossierStep)
+                .end()
                 .build();
     }
 }
